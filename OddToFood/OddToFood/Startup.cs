f78d69082f54d4ab92f0c62d41fca8fe679efee0
Alem Bristric <alem.bristric@gmail.com>
2018-11-14 14:56:11 +0100

    Default project template

---
+++ OddToFood/OddToFood/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OddToFood.Startup))]
+namespace OddToFood
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
