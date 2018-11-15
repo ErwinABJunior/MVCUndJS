4aba7ddf28c742a436a8e001d6d1b7e81ddc97fc
Alem Bristric <alem.bristric@gmail.com>
2018-11-15 15:53:44 +0100

    Some   JS  Testing ---

---
+++ OddToFood/OddToFood/app/js/common.js
@@ -0,0 +1,16 @@
+﻿
+
+
+var x = document.getElementById("calculate");
+
+
+b = 21;
+a = b * 2;
+
+//a = b * 2 + foo(c * 3);
+
+console.log(a);
+
+x.innerHTML = a;
+
+var age = prompt("What is your age?");
